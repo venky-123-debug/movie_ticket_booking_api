3095cb28c8a8e54190f7802e352da6dd021d366f
venkatesh c <venkat@print2block.com>
2024-04-19 22:39:51 +0530

    user login route created and working

--- routes/user.js
+++ routes/user.js
@@ -20,15 +20,9 @@ app.post("/signup", async (req, res) => {
 
     let userId = uuidv4()
     console.log(userId)
-    let tokenData = {
-      id: userId,
-    }
-    console.log({ tokenData })
 
-    let newUser = await user({ ...req.body, userId: userId }).save()
+    let newUser = await user({ ...req.body, password: SHA256(req.body.password).toString(), userId: userId }).save()
 
-    let token = await utilities.generateToken(tokenData, process.env.JWT_SECRET, 86400)
-    response.token = token
     response.data = newUser
     response.success = true
   } catch (error) {
@@ -42,27 +36,23 @@ app.post("/login", async (req, res) => {
   let response = {}
   response.success = false
   try {
-    if (!req.body.email) throw "No email"
-    if (!req.body.userName) throw "No userName"
+    if (!req.body.username) throw "No userName"
     if (!req.body.password) throw "No password"
-
-    let thisUser = await user.findOne({ email: req.body.email }).lean()
+    
+    let thisUser = await user.findOne({ username: req.body.username }).lean()
     if (thisUser == null) throw "No user"
+
+    console.log({ thisUser })
     if (thisUser.password != SHA256(req.body.password).toString()) throw "Invalid password"
-    let findBank = await institution.findOne({ institutionId: thisUser.organizationId }).lean()
-    console.log({ findBank })
-    if (!findBank) throw "No data found with the bank details."
+
     let tokenData = {
       id: thisUser.userId,
-      orgId: thisUser.organizationId,
-      bankName: findBank.name,
     }
     console.log({ tokenData })
 
     let token = await utilities.generateToken(tokenData, process.env.JWT_SECRET, 86400)
     response.token = token
     response.data = thisUser
-
     response.success = true
   } catch (error) {
     response = await errorhandler(error, response, req.originalUrl)
