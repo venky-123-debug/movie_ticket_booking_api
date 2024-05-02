eb870e76dcfa392742b3b74d76a8d15da7954041
venkatesh c <venkat@print2block.com>
2024-05-02 19:26:39 +0530

    movie schema changed

--- models/movie.js
+++ models/movie.js
@@ -1,27 +1,30 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const actorSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  image: { type: String, required: true }
-});
+  image: { type: String, required: true },
+  _id: false,
+})
 
 const crewSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   role: { type: String, required: true },
-});
+  _id: false,
+})
 
 const movieSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
-  genre: { type: [String] },
+  genre: { type: String },
+  language: { type: String },
+  Certification: { type: String },
   releaseDate: { type: Date },
   movieId: { type: String, unique: true },
   duration: { type: String },
-  language: { type: [String] },
   crew: { type: [crewSchema], required: true },
   actors: { type: [actorSchema], required: true },
-  poster: { type: String, description: "movie poster" }
-});
+  poster: { type: String, description: "movie poster" },
+})
 
-module.exports = mongoose.model("Movie", movieSchema);
+module.exports = mongoose.model("Movie", movieSchema)
